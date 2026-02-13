cc0d1f131a9b764de5973f415829d6bd8af14249
kemvagyok <0322sagibenedek@gmail.com>
2026-02-13 13:14:11 +0100

    updated

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn.functional as F
 # Metódusok kidolgozása
-import numpy as np
 from FewShotModel import FewShotModel
 from preprocessing import dataLoading_MNIST, traindatasetMasking
 from graph_tools import graph_creating
-
+#---
+import numpy as np
+import pandas as pd
 
 
 def main():
@@ -18,15 +19,17 @@ def main():
 	#p_n_vectors = [1,2,3]
 
 	epochs_max = 2500
-	epochs = np.arange(epochs_max+1, step=10)[1:] # Adott epoch-ban megnézi, hogy mennyire javult a modellt pontosség metrikával
+	epochs = np.arange(epochs_max+1, step=10)[1:] # Adott epoch-ban megnézi, hogy mennyire javult a modellt pontosság metrikával
 		
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 	train_x, train_y, test_x, test_y, class_num, channel_size = dataLoading_MNIST()
 	train_x_filtered, train_y_filtered = traindatasetMasking(train_x, train_y, class_num, 1)
 
-	test_x_filtered = test_x[:test_size]
-	test_y_filtered = test_y[:test_size]
+	train_x_filtered = train_x_filtered.to(device)
+	train_y_filtered = train_y_filtered.to(device)
+	test_x_filtered = test_x[:test_size].to(device)
+	test_y_filtered = test_y[:test_size].to(device)
 
 	best_results = []
 
@@ -34,8 +37,9 @@ def main():
 		print(max_label)
 		#Tanító halmaz maszkolása
 		train_x_filtered, train_y_filtered = traindatasetMasking(train_x, train_y, class_num, max_label)
-		train_test_x = torch.cat((train_x_filtered, test_x_filtered))
-		train_test_y = torch.cat((train_y_filtered, test_y_filtered))
+		
+		train_test_x = torch.cat((train_x_filtered.to(device), test_x_filtered.to(device)))
+		train_test_y = torch.cat((train_y_filtered.to(device), test_y_filtered.to(device)))
 
 		model = FewShotModel(input_size=28, output_size=class_num, latens_size = 64, channel_size = channel_size, device = device).to(device)
 		optimizer = torch.optim.Adam(model.parameters(), lr=lr)
@@ -45,24 +49,25 @@ def main():
 		for epoch in range(epochs_max+1):
 			model.train()
 			optimizer.zero_grad()
-			output = model(train_test_x.to(device), method_similar, 2)
-			loss = F.cross_entropy(output[:len(train_y_filtered)], train_y_filtered.to(device))
+			output = model(train_test_x.to(device), method_similar, 2).to(device)
+			loss = F.cross_entropy(output[:len(train_y_filtered)].to(device), train_y_filtered.to(device))
 			loss.backward()
 			optimizer.step()
 
 			if epoch in epochs:
 				with torch.no_grad():
 					model.eval()
-					out = model(test_x_filtered, method_similar, 2)
+					out = model(test_x_filtered, method_similar, 2).to(device)
 					pred = out.argmax(dim=1)
-					correct = pred.eq(test_y_filtered).sum().item()
+					correct = pred.eq(test_y_filtered.to(device)).sum().item()
 					acc = correct / len(test_y_filtered)
 					if acc > best_acc:
 						best_acc = acc
 						best_epoch = epoch
 
 		best_results.append((max_label, best_acc, best_epoch))
-
+	pd.DataFrame(best_results, columns=['Max Labeled Images per Class', 'Best Accuracy', 'Best Epoch']).to_csv('results.csv', index=False)
+		
 
 if "__main__"==__name__:
 	main()
